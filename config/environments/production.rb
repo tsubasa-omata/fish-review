d17446cf1806e4b9addac6e243baa199914765b8
tsubasa omata <runrunrun.108@gmail.com>
2019-09-16 18:07:36 +0900

    heroku　環境変数設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   #自分でtrueに変更した
   config.assets.compile = true
+  #自分で追加した
+  config.assets.initialize_on_precompile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
