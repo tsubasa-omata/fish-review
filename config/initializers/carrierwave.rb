a9a28e9ccd0235ac1457e36a3caf5b2c2296baa7
tsubasa omata <runrunrun.108@gmail.com>
2019-09-17 17:30:53 +0900

    a

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'AKIAXCNMTP35AVBG7NYV',
+    aws_access_key_id: '',
     aws_secret_access_key: '',
     region: 'ap-northeast-1'
     }
