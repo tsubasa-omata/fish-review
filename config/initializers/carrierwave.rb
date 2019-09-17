daae76ab800a03727f151c5be2e33307aed32fc0
tsubasa omata <runrunrun.108@gmail.com>
2019-09-17 16:47:31 +0900

    change s3 key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'AKIAXCNMTP35H2JP35N4',
-    aws_secret_access_key: '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',
+    aws_access_key_id: 'AKIAXCNMTP35MXYKN2MA',
+    aws_secret_access_key: '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',
     region: 'ap-northeast-1'
     }
 
