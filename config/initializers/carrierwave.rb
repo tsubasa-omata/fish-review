d9916a7cb2c4b7a1d41a612c174af25417ef7d48
tsubasa omata <runrunrun.108@gmail.com>
2019-09-17 20:35:24 +0900

    s3のキーをdotenvで定数化した

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'aaa',
-    aws_secret_access_key: '',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECLET_ACCESS_KEY'],
     region: 'ap-northeast-1'
     }
 
